ed1a5772aa31e9233fa5e03aba1ae877eeac6c5b
Alex Knyazev <alexkniazev@gmail.com>
2025-09-04 16:16:05 -0400

    Cleanup

--- stellgap/__init__.py
+++ stellgap/__init__.py
@@ -6,7 +6,6 @@ from .continuum import AE3DEigenvector, Harmonic #outputs from AE3D
 from .continuum import FAR3DEigenproblem
 from .continuum import plot_continuum, plot_condition_numbers, data_from_dir, plot_ae3d_eigenmode, plot_continua, continuum_from_ae3d
 from .continuum import continuum_from_dir
-from .ae3d_eigensolver import AE3DEigensolver
 try:
     from .simsopt import saw_from_ae3d
 except ImportError:
